2e530d447bf332c8ec250114745c50aada43229f
PierreChetelat <pierre.chetelat@he-arc.ch>
2015-02-16 11:42:56 +0100

    calcule distance ville et solution, creation population random, elitisme

--- ChetelatLaville.py
+++ ChetelatLaville.py
@@ -1,145 +1,176 @@
 __author__ = 'Chetelat Laville'
 
+import math
+from random import shuffle
+
+nbSolution = 10
+
 class Ville:
-    def __init__(self, name, pos):
-        self.__name = name
-        self.__pos = pos
-        self.__x = pos[0]
-        self.__y = pos[1]
+   def __init__(self, name, pos):
+      self.__name = name
+      self.__pos = pos
+      self.__x = pos[0]
+      self.__y = pos[1]
 
-    def name(self):
-        return self.__name
+   def name(self):
+      return self.__name
 
-    def pos(self):
-        return self.__pos
+   def pos(self):
+      return self.__pos
 
-    def x(self):
-        return self.__x
+   def x(self):
+      return self.__x
 
-    def y(self):
-        return self.__y
+   def y(self):
+      return self.__y
 
-    def __repr__(self):
-        return "'%s' (%d, %d)" % (self.__name, self.__x, self.__y)
-    def distance(self, other):
-        #todo
-        return 0
-    def mutate(self):
-        #todo
-        return None
+   def __repr__(self):
+      return "'%s' (%d, %d)" % (self.__name, self.__x, self.__y)
+
+   def distance(self, other):
+      distance1 = math.sqrt(self.__x * self.__x + self.__y*self.__y)
+      distance2 = math.sqrt(other.__x * other.__x + other.__y*other.__y)
+      return math.fabs(distance1 - distance2)
+
+def mutate(self):
+   #todo
+   return None
 
 class Solution:
-    def __init__(self, villes):
-        self.__villes = villes
-        self.__distance = 0
-    def distance(self):
-        #todo
-        pass
+   def __init__(self, villes):
+      self.__villes = villes
+      self.__distance = 0
+   def distance(self):
+      distance = 0
+      precedente = self.__villes[-1]
+      for ville in self.__villes:
+         distance += ville.distance(precedente)
+         precedente = ville
+      self.__distance = distance
+      return self.__distance
 
+def construirePopulation(villes):
+   solutions = []
+   solutions.append(Solution(list(villes)))
+   for i in range(nbSolution-1):
+      villesSolution = list(villes)
+      shuffle(villesSolution)
+      solutions.append(Solution(villesSolution))
 
+def elitisme(solutions):
+   def comp(sol1,sol2):
+      if sol1.distance < sol2.distance:
+         return -1
+      elif sol1.distance > sol2.distance:
+         return 1
+      else:
+         return 0
+
+   solutionsSorted = list(solutions)
+   solutionsSorted.sort(comp)
+   return solutionsSorted[0:len(solutionsSorted)/2]
 
 def ga_solve(file=None, gui=True, maxtime=0):
-    import pygame
-    from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
-    import sys
+   import pygame
+   from pygame.locals import KEYDOWN, QUIT, MOUSEBUTTONDOWN, K_RETURN, K_ESCAPE
+   import sys
 
+   screen_x = 500
+   screen_y = 500
 
-    screen_x = 500
-    screen_y = 500
+   city_color = [10,10,200] # blue
+   city_radius = 3
 
-    city_color = [10,10,200] # blue
-    city_radius = 3
+   font_color = [255,255,255] # white
 
-    font_color = [255,255,255] # white
+   pygame.init()
+   window = pygame.display.set_mode((screen_x, screen_y))
+   pygame.display.set_caption('Exemple')
+   screen = pygame.display.get_surface()
+   font = pygame.font.Font(None,30)
+   font_city = pygame.font.Font(None,20)
 
-    pygame.init()
-    window = pygame.display.set_mode((screen_x, screen_y))
-    pygame.display.set_caption('Exemple')
-    screen = pygame.display.get_surface()
-    font = pygame.font.Font(None,30)
-    font_city = pygame.font.Font(None,20)
+   def draw(villes):
+      screen.fill(0)
+      for ville in villes:
+         screen.blit(font_city.render(str(ville), True, font_color, ), ville.pos())
+         pygame.draw.circle(screen,city_color,ville.pos(),city_radius)
+      text = font.render("Nombre: %i" % len(villes), True, font_color)
+      textRect = text.get_rect()
+      screen.blit(text, textRect)
+      pygame.display.flip()
 
-    def draw(villes):
-        screen.fill(0)
-        for ville in villes:
-            screen.blit(font_city.render(str(ville), True, font_color, ), ville.pos())
-            pygame.draw.circle(screen,city_color,ville.pos(),city_radius)
-        text = font.render("Nombre: %i" % len(villes), True, font_color)
-        textRect = text.get_rect()
-        screen.blit(text, textRect)
-        pygame.display.flip()
+   cities = []
+   draw(cities)
 
-    cities = []
-    draw(cities)
+   if file:
+      #todo loades cities from file
+      fileReader = open(file, "r")
+      for line in fileReader:
+         line = line.replace("\n", "")
+         print("Line : " + line)
+         parts = line.split(" ")
+         pos = (int(parts[1]), int(parts[2]))
+         cities.append(Ville(parts[0], pos))
+         pass
+      print(cities)
+      fileReader.close()
+   else:
+      citiesIndex = 0
+      collecting = True
+      while collecting:
+         for event in pygame.event.get():
+            if event.type == QUIT:
+               sys.exit(0)
+            elif event.type == KEYDOWN and event.key == K_RETURN:
+               collecting = False
+            elif event.type == MOUSEBUTTONDOWN:
+               citiesIndex += 1
+               cities.append(Ville("v%d" % citiesIndex, pygame.mouse.get_pos()))
+               draw(cities)
+   if len(cities) == 0:
+      print("Pas de villes !")
+      #todo MggBox
+      sys.exit(0)
 
-    if file:
-        #todo loades cities from file
-        fileReader = open(file, "r")
-        for line in fileReader:
-            line = line.replace("\n", "")
-            print("Line : " + line)
-            parts = line.split(" ")
-            pos = (int(parts[1]), int(parts[2]))
-            cities.append(Ville(parts[0], pos))
-            pass
-        print(cities)
-        fileReader.close()
-    else:
-        citiesIndex = 0
-        collecting = True
-        while collecting:
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    sys.exit(0)
-                elif event.type == KEYDOWN and event.key == K_RETURN:
-                    collecting = False
-                elif event.type == MOUSEBUTTONDOWN:
-                    citiesIndex += 1
-                    cities.append(Ville("v%d" % citiesIndex, pygame.mouse.get_pos()))
-                    draw(cities)
-    if len(cities) == 0:
-        print("Pas de villes !")
-        #todo MggBox
-        sys.exit(0)
+   screen.fill(0)
+   for ville in cities:
+      screen.blit(font_city.render(str(ville), True, font_color, ), ville.pos())
+      pygame.draw.circle(screen,city_color,ville.pos(),city_radius)
+   pygame.draw.lines(screen,city_color,True,[city.pos() for city in cities])
+   text = font.render("Un chemin, pas le meilleur!", True, font_color)
+   textRect = text.get_rect()
+   screen.blit(text, textRect)
+   pygame.display.flip()
 
-    screen.fill(0)
-    for ville in cities:
-        screen.blit(font_city.render(str(ville), True, font_color, ), ville.pos())
-        pygame.draw.circle(screen,city_color,ville.pos(),city_radius)
-    pygame.draw.lines(screen,city_color,True,[city.pos() for city in cities])
-    text = font.render("Un chemin, pas le meilleur!", True, font_color)
-    textRect = text.get_rect()
-    screen.blit(text, textRect)
-    pygame.display.flip()
-
-    while True:
-        event = pygame.event.wait()
-        if event.type == KEYDOWN or event.type == QUIT: break
+   while True:
+      event = pygame.event.wait()
+      if event.type == KEYDOWN or event.type == QUIT: break
 
 def main():
-    import sys
-    gui = None
-    maxtime = None
-    filename = None
-    nextismaxtime = False
-    for arg in sys.argv[1:]:
-        print("arg = '%s'" % arg)
-        if arg == "--no-gui":
-            nextismaxtime = False
-            gui = False
-        elif arg == "--maxTime":
-            nextismaxtime = True
-        elif nextismaxtime:
-            maxtime = int(arg)
-            nextismaxtime = False
-        else:
-            if filename:
-                print("Two filename define !")
-                sys.exit(0)
-            filename = arg
-    ga_solve(filename, gui, maxtime)
+   import sys
+   gui = None
+   maxtime = None
+   filename = None
+   nextismaxtime = False
+   for arg in sys.argv[1:]:
+      print("arg = '%s'" % arg)
+      if arg == "--no-gui":
+         nextismaxtime = False
+         gui = False
+      elif arg == "--maxTime":
+         nextismaxtime = True
+      elif nextismaxtime:
+         maxtime = int(arg)
+         nextismaxtime = False
+      else:
+         if filename:
+            print("Two filename define !")
+            sys.exit(0)
+         filename = arg
+   ga_solve(filename, gui, maxtime)
 
 
 
 if __name__ == "__main__":
-    main()
+   main()
